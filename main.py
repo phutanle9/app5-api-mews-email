792e3538e61d3370bb5407c4ada9769989d5dc94
phutanle9 <135482723+phutanle9@users.noreply.github.com>
2025-03-14 00:54:17 +0700

    update send mail

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import requests
+from send_email import send_email
 
-url= "https://newsapi.org/v2/everything?q=tesla&from=2025-02-13&sortBy=publishedAt&apiKey=7f280d9389184fdd99c22cbce8a66742"
+api_key = "7f280d9389184fdd99c22cbce8a66742"
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-02-13&sortBy=publishedAt&apiKey={api_key}"
 
 request = requests.get(url)
-content= request.json()
+content = request.json()
 
-for article in content['articles']:
-    print(article['title'])
-    print(article["description"])
+body = ""
+for article in content["articles"]:
+    # Use empty string if title or description is None
+    body = body + (article['title'] or "") + "\n" + (article["description"] or "") + "\n\n"
+
+# Encode body in UTF-8
+body = body.encode("utf-8")
+
+# Send email if body is not empty
+if body:
+    send_email(message=body)
